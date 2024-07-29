a7ca08c4c0d06246c12b93a5549852c6d5b7e971
orcus <orcus20210925@gmail.com>
2024-07-30 01:33:34 +0800

    refactor(getCommitMessageFromGemini): remove unnecessary prompt generation

--- utils/getCommitMessageFromGemini.js
+++ utils/getCommitMessageFromGemini.js
@@ -1,48 +1,4 @@
-const getPrompt = (changes) => {
-  const prompt = ` **You are a professional code analyst.**
-
-  Please write a concise and meaningful Git commit message for the following file changes in the specified format:
-
-  **<type>(<scope>): <subject>**
-
-  Where:
-  * **type** is one of: build, chore, feat, fix, docs, refactor, perf, style, test, ci, revert\n
-  - feat: a new feature is introduced with the changes
-  - fix: a bug fix has occurred
-  - chore: changes that do not relate to a fix or feature and don't modify src or test files (for example updating dependencies)
-  - refactor: refactored code that neither fixes a bug nor adds a feature
-  - docs: updates to documentation such as a the README or other markdown files
-  - style: changes that do not affect the meaning of the code, likely related to code formatting such as white-space, missing semi-colons, and so on.
-  - test: including new or correcting previous tests
-  - perf: performance improvements
-  - ci: continuous integration related
-  - build: changes that affect the build system or external dependencies
-  - revert: reverts a previous commit
-  * **scope** is optional and denotes the part of the code affected:\n
-  -	If only one file is affected, use the only file name as the scope.\n
-  -	If multiple files are affected, leave the scope blank.\n
-  * **subject** is a concise description of the change\n
-  - The first letter of the subject should be lowercase.\n
-
-  Examples:
-
-  Good:
-  feat(index): improve performance with lazy load implementation for images
-  chore: update npm dependency to latest version
-
-  Bad:
-  feat(index, other): improve performance with lazy load implementation for images
-  feat(src/feature/components/SomeComponent): improve performance with lazy load implementation for images
-
-  Do not include any additional description or explanation.
-  Format your response exactly as:\nGit commit message: <type>(<scope>): <subject>
-
-  File changes:
-  \n${changes}\n
-  `;
-
-  return prompt;
-};
+const getPrompt = require('./getPrompt');
 
 async function getCommitMessageFromGemini(changes, model) {
   const prompt = getPrompt(changes);
